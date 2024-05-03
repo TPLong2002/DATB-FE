559dfb1dcf9d47085530ee296766ea3ef3acc9ed
TPLong2002 <tranphilong2301@gmail.com>
2024-05-03 22:49:01 +0700

    thêm chức năng xem người thân

--- src/components/pages/profile/index.js
+++ src/components/pages/profile/index.js
@@ -1,55 +1,132 @@
 import React, { useEffect, useState } from "react";
-import { getProfile } from "@/services/profile";
+import {
+  getProfile,
+  updateProfile,
+  getRelativesProfile,
+} from "@/services/profile";
 import { useLocation } from "react-router-dom";
-import { Button, Input, Avatar, DatePicker } from "antd";
+import { Button, Input, Avatar, DatePicker, Radio } from "antd";
 import { getGroupByUserId } from "@/services/group";
-import { format } from "date-fns";
 import dayjs from "dayjs";
 
 function Profile() {
   let { state } = useLocation();
   const format = "YYYY/MM/DD";
-  const [profile, setProfile] = useState({});
+  const [profile, setProfile] = useState([{}]);
   const [group, setGroup] = useState({});
+  const [options, setOptions] = useState(1);
   const fetchProfile = async (id) => {
-    const res = await getProfile(id);
     const group = await getGroupByUserId(id);
-    setProfile(res.data);
     setGroup(group.data.Group);
+    if (options === 1) {
+      const res = await getProfile(id);
+      setProfile(res.data);
+    }
+    if (options === 2) {
+      const res = await getRelativesProfile(id);
+      setProfile(res.data);
+    }
   };
   useEffect(() => {
     fetchProfile(state.id);
-  }, []);
+  }, [state.id, options]);
+  const onChange = (date, dateString) => {
+    setProfile({ ...profile, dateOfBirth: dateString });
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProfile({ ...profile, [name]: value });
+  };
+  const onSubmit = async () => {
+    console.log(profile);
+    const res = await updateProfile(profile);
+    if (res.code === 0) {
+      console.log(res.message);
+    }
+  };
   console.log(profile);
   return (
     <div className="flex flex-col space-y-3">
       <div className="mt-3 space-x-2">
-        <Button>thông tin cá nhân</Button>
-        {group.name === "student" && <Button>thông tin phụ huynh</Button>}
-        {group.name === "parent" && <Button>thông tin học sinh</Button>}
-      </div>
-      <div className="border rounded-sm border-gray-400">
-        <Avatar
-          shape="square"
-          size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-          src={profile.avt}
-        />
-        <div>
-          <Input value={profile?.firstName}></Input>
-          <Input value={profile?.lastName}></Input>
-          <Input value={profile?.email}></Input>
-          <Input value={profile?.phoneNumber}></Input>
-          <Input value={profile?.address}></Input>
-          <Input value={profile?.CCCD}></Input>
-          {!profile?.dateOfBirth && <DatePicker format="DD-MM-YYYY" />}
-          {profile?.dateOfBirth && (
-            <DatePicker
-              defaultValue={dayjs(profile.dateOfBirth, format)}
-              format={format}
-            />
+        <Radio.Group value={options}>
+          <Radio.Button value={1} onClick={() => setOptions(1)}>
+            thông tin cá nhân
+          </Radio.Button>
+          {group.name === "student" && (
+            <Radio.Button value={2} onClick={() => setOptions(2)}>
+              thông tin phụ huynh
+            </Radio.Button>
           )}
-        </div>
+          {group.name === "parent" && (
+            <Radio.Button value={2} onClick={() => setOptions(2)}>
+              thông tin học sinh
+            </Radio.Button>
+          )}
+        </Radio.Group>
       </div>
+      {profile.map((profile, index) => (
+        <div className="border rounded-sm border-gray-400" key={index}>
+          <Avatar
+            shape="square"
+            size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+            src={profile?.avt}
+            className="border rounded-sm border-gray-400"
+          />
+          <div>
+            <Input
+              value={profile?.firstName}
+              name="firstName"
+              onChange={handleChange}
+              placeholder="Thêm họ"
+            ></Input>
+            <Input
+              value={profile?.lastName}
+              name="lastName"
+              onChange={handleChange}
+              placeholder="Thêm tên"
+            ></Input>
+            <Input
+              value={profile?.email}
+              name="email"
+              onChange={handleChange}
+              placeholder="Thêm email"
+            ></Input>
+            <Input
+              value={profile?.phoneNumber}
+              name="phoneNumber"
+              onChange={handleChange}
+              placeholder="Thêm số điện thoại"
+            ></Input>
+            <Input
+              value={profile?.address}
+              name="address"
+              onChange={handleChange}
+              placeholder="Thêm địa chỉ"
+            ></Input>
+            <Input
+              value={profile?.CCCD}
+              name="CCCD"
+              onChange={handleChange}
+              placeholder="Thêm CCCD"
+            ></Input>
+            {!profile?.dateOfBirth && (
+              <DatePicker format="DD-MM-YYYY" onChange={onChange} />
+            )}
+            {profile?.dateOfBirth && (
+              <DatePicker
+                defaultValue={dayjs(profile.dateOfBirth, format)}
+                format={format}
+                onChange={onChange}
+              />
+            )}
+          </div>
+          <div className="text-center">
+            <Button type="primary" onClick={onSubmit}>
+              Save
+            </Button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/services/profile/index.js
+++ src/services/profile/index.js
@@ -7,3 +7,19 @@ export const getProfile = async (id) => {
     console.log(error);
   }
 };
+export const updateProfile = async (data) => {
+  try {
+    const res = await Axios.put("/profile", data);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const getRelativesProfile = async (id) => {
+  try {
+    const res = await Axios.get("/profile/relatives", { params: { id } });
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
